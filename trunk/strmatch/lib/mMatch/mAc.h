4bd739941a9a91cb3882b348f1ff3db551cb1e7a
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2009-10-13 13:48:09 +0000

    template mAc

--- trunk/strmatch/lib/mMatch/mAc.h
+++ trunk/strmatch/lib/mMatch/mAc.h
@@ -19,45 +19,64 @@
 #include <stdlib.h>
 #include <list>
 #include "mMatch.h"
-#define CHAR_SET 256
+//#define CHAR_SET 256
 using namespace std;
 namespace dmMatch{
 
+template<int CHAR_SET>
 class acNode
 {
 	public:
 		int patID;
-		acNode* go[256];
+		acNode* go[CHAR_SET];
 		acNode* failure;
 	public:
 		acNode(){memset(this, 0, sizeof(acNode)); patID=-1;}
 		int isMatched(){ return patID != -1;}
 };
-typedef acNode* acNodeP;
 
+template<int CHAR_SET>
+class acNodeShort
+{
+	public:
+		I16 patID;
+		U16 failure;
+		U16 go[CHAR_SET];
+	public:
+		acNodeShort(){memset(this, 0, sizeof(acNodeShort)); patID=-1;}
+		int isMatched(){ return patID != -1;}
+};
+
+
+template<int CHAR_SET>
 class AcNodeStore
 {
+    public:
+        typedef acNode<CHAR_SET>* acNodeP;
 	public:
 		acNodeP p_NodeList;//! root is the first element
 		int nodeNum;
 		int type;
 };
 
+template<int CHAR_SET=256>
 class mAc:public mMatch
 {
-	private:
-		acNodeP pRoot;
-		AcNodeStore acNodeStore;
-		list<acNodeP> nodeList;
-	public:
-		mAc();
-		mAc(char** pat, int n);
-		virtual int search(char* txt, int n){};
-		virtual int search(char* txt);
-	protected:
-		virtual void init();
-	private:	
-		acNodeP makeNode() { acNodeP newNode= new acNode(); nodeList.push_back(newNode); return newNode;};
+    public:
+        typedef acNode<CHAR_SET>* acNodeP;
+    private:
+        acNodeP pRoot;
+        AcNodeStore<CHAR_SET> acNodeStore;
+        list<acNodeP> nodeList;
+    public:
+        mAc();
+        mAc(char** pat, int n);
+        virtual int search(char* txt, int n){};
+        virtual int search(char* txt);
+    protected:
+        virtual void init();
+    private:	
+        acNodeP makeNode() { acNodeP newNode= new acNode<CHAR_SET>(); nodeList.push_back(newNode); return newNode;};
 };
 
 };
