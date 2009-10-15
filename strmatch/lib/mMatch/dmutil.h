0643d3daad6a3110e7d4eb3b1b610e2c7b81dfd9
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2009-10-15 14:12:48 +0000

    add gene search

---
+++ strmatch/lib/mMatch/dmutil.h
@@ -0,0 +1,61 @@
+/*
+ * =====================================================================================
+ *Copyright(C) by Zhenghua Dai. All rights reserved.
+ *
+ *
+ *
+ * =====================================================================================
+ * =====================================================================================
+ *
+ *       Filename:  util.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2009年10月15日 19时01分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zhenghua Dai (Zhenghua Dai), djx.zhenghua@gmail.com
+ *        Company:  dzh
+ *
+ * =====================================================================================
+ */
+#ifndef  UTIL_HEADER__INC
+#define  UTIL_HEADER__INC
+struct Pattern_s{
+    char* str;
+    int len;
+};
+typedef struct Pattern_s Pattern_t;
+
+#ifdef __cplusplus
+inline
+#else
+static
+#endif
+unsigned char num2agct(unsigned char n){
+    static unsigned char code[4]={'A','G','C','T'};
+    if(n>4) return 'N';
+    return code[n];
+}
+
+#ifdef __cplusplus
+inline
+#else
+static
+#endif
+unsigned char agct2num(unsigned char bp){
+    switch(bp){
+        case 'a': case'A': return 0 ;
+        case 'g': case'G': return 1 ;
+        case 'c': case'C': return 2 ;
+        case 't': case'T': return 3 ;
+        default: return 4;
+    }
+}
+// a 97  0110 0001       00
+// g 103 0110 0111       01
+// c 99  0110 0011       10
+// t 116 0111 0100       11
+#endif   /* ----- #ifndef UTIL_HEADER__INC  ----- */
--- strmatch/test/extract.c
+++ strmatch/test/extract.c
@@ -5,118 +5,118 @@
 
 char * Getsubjectfromfile(char *pfname)
 {
-	int psubstep=0,buffcounts;
-  //      unsigned int temp;
-	char * psub;
-	char temp2;
-	FILE *pf;	
-	struct stat filestat;
-        if((pf=fopen(pfname,"r"))==NULL)
-	{
-	printf("cannot open input string");	
-	exit(0);
-	}
-     buffcounts=stat(pfname,&filestat);
-     buffcounts=filestat.st_size;
+    int psubstep=0,buffcounts;
+    //      unsigned int temp;
+    char * psub;
+    char temp2;
+    FILE *pf;	
+    struct stat filestat;
+    if((pf=fopen(pfname,"r"))==NULL)
+    {
+        printf("cannot open input string");	
+        exit(0);
+    }
+    buffcounts=stat(pfname,&filestat);
+    buffcounts=filestat.st_size;
 
-/*	//获取文件长度 elementcounts*/
-   if (( psub=(char *) malloc(sizeof(char) *(buffcounts+1)))==NULL)
-   {
-      printf("failure mallocaa      ");
+    /*	//获取文件长度 elementcounts*/
+    if (( psub=(char *) malloc(sizeof(char) *(buffcounts+1)))==NULL)
+    {
+        printf("failure mallocaa      ");
 
-   }
-   
+    }
+
     //  printf("succuss malloc");
     while((temp2=fgetc(pf))!=EOF)
-	{
-//		temp=(unsigned int )temp2;
-//                     if(((temp>=97)&&(temp<=122))||((temp>=65)&&(temp<=90)))
-                      if((temp2=='\0'))continue;
-			{psub[psubstep]=temp2;
-		  psubstep++;
-			}
-	}
-psub[psubstep]='\0';
-return psub;
+    {
+        //		temp=(unsigned int )temp2;
+        //                     if(((temp>=97)&&(temp<=122))||((temp>=65)&&(temp<=90)))
+        if((temp2=='\0'))continue;
+        {psub[psubstep]=temp2;
+            psubstep++;
+        }
+    }
+    psub[psubstep]='\0';
+    return psub;
 
 }
 int   Getpatternfromfile(char *pfname,char **patts)
 {
-	int psubstep=0,buffcounts;
-  //      unsigned int temp;
-	char buff[256];
-	char * psub;
-	char temp2;
-	int patno=0,i;
-	FILE *pf;	
-        if((pf=fopen(pfname,"r"))==NULL)
-	{
-	printf("cannot open input string");	
-	exit(0);
-	}
-    
+    int psubstep=0,buffcounts;
+    //      unsigned int temp;
+    char buff[256];
+    char * psub;
+    char temp2;
+    int patno=0,i;
+    FILE *pf;	
+    if((pf=fopen(pfname,"r"))==NULL)
+    {
+        printf("cannot open input string");	
+        exit(0);
+    }
+
     while((temp2=fgetc(pf))!=EOF)
-	{
-//		temp=(unsigned int )temp2;
-//                     if(((temp>=97)&&(temp<=122))||((temp>=65)&&(temp<=90)))
-                      if(temp2=='\n')
-		      {
-			      buff[psubstep]='\0';
-			      psub=(char *)malloc(psubstep*sizeof(char));
-			      for(i=0;i<=psubstep;i++)
-			      {
-				      psub[i]=buff[i];
-			      }
-			      patts[patno++]=psub;
-			      psubstep=0;
-//				  printf("%c\n",fgetc(pf));
-			 //    printf("%s\n",patts[patno]); 
-			      continue;
-		      }
-		      else
-			{buff[psubstep]=temp2;
-			//	printf("%c",temp2);
-		  psubstep++;
-			}
-	}
-return patno;
+    {
+        //		temp=(unsigned int )temp2;
+        //                     if(((temp>=97)&&(temp<=122))||((temp>=65)&&(temp<=90)))
+        if(temp2=='\n')
+        {
+            buff[psubstep]='\0';
+            psub=(char *)malloc(psubstep*sizeof(char));
+            for(i=0;i<=psubstep;i++)
+            {
+                psub[i]=buff[i];
+            }
+            patts[patno++]=psub;
+            psubstep=0;
+            //				  printf("%c\n",fgetc(pf));
+            //    printf("%s\n",patts[patno]); 
+            continue;
+        }
+        else
+        {buff[psubstep]=temp2;
+            //	printf("%c",temp2);
+            psubstep++;
+        }
+    }
+    return patno;
 
 }
 
 
 char * Getgeneseqfromfile(char *pfname)
 {
-	int psubstep=0,buffcounts;
-        unsigned int temp;
-	char * psub;
-	char temp2;
-	FILE *pf;	
-	struct stat filestat;
-        if((pf=fopen(pfname,"r"))==NULL)
-	{
-	printf("cannot open input string");	
-	exit(0);
-	}
-     buffcounts=stat(pfname,&filestat);
-     buffcounts=filestat.st_size;
+    int psubstep=0,buffcounts;
+    unsigned int temp;
+    char * psub;
+    char temp2;
+    FILE *pf;	
+    struct stat filestat;
+    if((pf=fopen(pfname,"r"))==NULL)
+    {
+        printf("cannot open input string");	
+        exit(0);
+    }
+    buffcounts=stat(pfname,&filestat);
+    buffcounts=filestat.st_size;
 
-/*	//获取文件长度 elementcounts*/
-   if (( psub=(char *) malloc(sizeof(char) *(buffcounts+1)))==NULL)
-   {
-      printf("failure mallocaa      ");
-      exit(0);
+    /*	//获取文件长度 elementcounts*/
+    if (( psub=(char *) malloc(sizeof(char) *(buffcounts+1)))==NULL)
+    {
+        printf("failure mallocaa      ");
+        exit(0);
 
-   }
-   
+    }
+
     //  printf("succuss malloc");
     while((temp2=fgetc(pf))!=EOF)
-	{
-                     if((temp2=='A')||(temp2=='G')||(temp2=='C')||(temp2=='T')||(temp2=='a')||(temp2=='g')||(temp2=='c')||(temp2=='t'))
-			{psub[psubstep]=temp2;
-		  psubstep++;
-			}
-	}
-psub[psubstep]=0;
-return psub;
+    {
+        if((temp2=='A')||(temp2=='G')||(temp2=='C')||(temp2=='T')||(temp2=='a')||(temp2=='g')||(temp2=='c')||(temp2=='t'))
+        {psub[psubstep]=temp2;
+            psubstep++;
+        }
+    }
+    psub[psubstep]=0;
+    return psub;
 
 }
