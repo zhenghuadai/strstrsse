eb87c18de309c0d451e1ab886cb0bd7679ff3fbb
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2009-10-15 12:59:21 +0000

    matched pattern list

--- trunk/strmatch/lib/mMatch/mMatch.h
+++ trunk/strmatch/lib/mMatch/mMatch.h
@@ -18,36 +18,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <list>
+#include "dmutil.h"
+using namespace std;
 namespace dmMatch{
 typedef unsigned char Uchar;
 typedef unsigned short U16;
 typedef short I16;
 typedef enum{
-mAC,mWM
+    mAC,mWM
 } mAlgtype;
 
 typedef int (* reportFunc)(int patID, int idx);
 class mMatch{
-	public:
-		mMatch(char** pat, int patNum){memset(this, 0, sizeof(mMatch)); init(pat, patNum); report=reportSilent;};
-	public:
-		virtual int search(char* txt, int n){};
-		virtual int search(char* txt){};
-		void setReportFunc(reportFunc f){report = f;}
-		void init(char** ,int n);
-	protected:
-		virtual void compile(){};
-	private:
-		void clean();
-	public:
-		static int reportDefault(int patid, int idx){ printf("(%d,%d) ", idx, patid);}
-		static int reportSilent(int patid, int idx){}
-	protected:
-		int type;
-		char** mPatterns;
-		int* mPatLen;
-		int mPatNum;
-		reportFunc report;
-
+    public:
+        mMatch(char** pat, int patNum){memset(this, 0, sizeof(mMatch) - sizeof(list<Pattern_t>)); init(pat, patNum); report=reportSilent;};
+    public:
+        virtual int search(char* txt, int n){};
+        virtual int search(char* txt){};
+        void setReportFunc(reportFunc f){report = f;}
+        void init(char** ,int n);
+        ~mMatch(){}
+    protected:
+        virtual void compile(){};
+    private:
+        void clean();
+    public:
+        static int reportDefault(int patid, int idx){ printf("(%d,%d) ", idx, patid);}
+        static int reportSilent(int patid, int idx){}
+    protected:
+        int type;
+        char** mPatterns;
+        int* mPatLen;
+        int mPatNum;
+        reportFunc report;
+        list<Pattern_t> pPatList;
 };
 }
