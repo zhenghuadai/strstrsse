4317c04395f88e3d99ace7a1d65f4ced1c2f4e78
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2009-11-06 13:53:06 +0000

    test

--- rdtsc.h
+++ rdtsc.h
@@ -10,39 +10,39 @@ static inline  double rdtscdiff(unsigned int low1,unsigned int high1,unsigned in
 {
 	const  double MAXintplus1 = 4294967296.0;
 	unsigned int low,high;
-	if (low2<low1) {
-	low = 0xffffffff - low1 + low2;
-	high = high2 - high1 -1;
-	}
-	else {
-	high = high2 - high1;
-	low = low2 - low1;
-	}
-//	printf("%d,",low);
-	return (double)high * MAXintplus1 + (double)low ;
+    if (low2<low1) {
+        low = 0xffffffff - low1 + low2;
+        high = high2 - high1 -1;
+    }
+    else {
+        high = high2 - high1;
+        low = low2 - low1;
+    }
+    //	printf("%d,",low);
+    return (double)high * MAXintplus1 + (double)low ;
 }
 static inline double mdtime(int id)
 {
-	static unsigned int high0,low0,high1,low1;
-	if(id){
-		rdtsc(low1,high1);
-	        return rdtscdiff(low0,high0,low1,high1)/rdtscfreq;	
-	}
-	else{
-		rdtsc(low0,high0);
-		return 0.0;
-	}
+    static unsigned int high0,low0,high1,low1;
+    if(id){
+        rdtsc(low1,high1);
+        return rdtscdiff(low0,high0,low1,high1)/rdtscfreq;	
+    }
+    else{
+        rdtsc(low0,high0);
+        return 0.0;
+    }
 }
 static inline double mdtime2(int id)
 {
-	static unsigned int high0,low0,high1,low1;
-	if(id){
-		rdtsc(low1,high1);
-	        return rdtscdiff(low0,high0,low1,high1)/rdtscfreq;	
-	}
-	else{
-		rdtsc(low0,high0);
-		return 0.0;
-	}
+    static unsigned int high0,low0,high1,low1;
+    if(id){
+        rdtsc(low1,high1);
+        return rdtscdiff(low0,high0,low1,high1)/rdtscfreq;	
+    }
+    else{
+        rdtsc(low0,high0);
+        return 0.0;
+    }
 }
 #endif
---
+++ strmatch/lib/report.h
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *Copyright(C) by Zhenghua Dai. All rights reserved.
+ *
+ *
+ *
+ * ==================================
+ * ==================================
+ *
+ *       Filename:  report.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2009年11月06日 21时40分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zhenghua Dai (Zhenghua Dai), djx.zhenghua@gmail.com
+ *        Company:  dzh
+ *
+ * =====================================================================================
+ */
+
+#ifndef  REPORT_INC
+#define  REPORT_INC
+
+typedef int (*reportFunc)(char* text, int idx, char* pat);
+void setReportFunc(reportFunc rf);
+#define SEARCH_STOP 1
+#define SEARCH_CONTINUE 0
+#define SEARCH_THE_FIRST ((reportFunc) 1)
+#define SEARCH_ALL ((reportFunc) 2)
+#define SEARCH_SILENT ((reportFunc) 3)
+
+extern reportFunc report_function ;
+#endif   /* ----- #ifndef REPORT_INC  ----- */
