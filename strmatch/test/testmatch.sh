5566934b415b79ea6b26377058b8840b34e422e4
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2010-01-22 09:52:49 +0000

    test script

--- strmatch/test/testmatch.sh
+++ strmatch/test/testmatch.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+TestCase=$1
 echo "" > testmatch.res
+# test on alphabet
+if [ "$TestCase" == "alpha" ] || [ -z $TestCase ]; then
 ./match -t MMitchell.Gonewiththewind.txt -a the >> testmatch.res
 ./match -t MMitchell.Gonewiththewind.txt -a the >> testmatch.res
 ./match -t MMitchell.Gonewiththewind.txt -a the >> testmatch.res
@@ -11,6 +15,15 @@ echo "" > testmatch.res
 ./match -t MMitchell.Gonewiththewind.txt -a wholeheartedness >> testmatch.res
 
 
+
+./match -t MMitchell.Gonewiththewind.txt -a "tomorrow is another day" -v 0 >> testmatch.res
+./match -t MMitchell.Gonewiththewind.txt -a "My dear, I don't give a damn" -v 0 >> testmatch.res
+./match -t MMitchell.Gonewiththewind.txt -a "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" -v 0 >> testmatch.res
+./match -t MMitchell.Gonewiththewind.txt -a "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" -v 0 >> testmatch.res
+./match -t MMitchell.Gonewiththewind.txt -a "000000000000000000000000000000000" -v 0 >> testmatch.res
+fi
+#test on genome
+if [ TestCase == "genome" ] || [ -z $TestCase ]; then
 ./match -t genome/chr1.fa -a TATAA >> testmatch.res
 ./match -t genome/chr1.fa -a TTGACA >> testmatch.res
 ./match -t genome/chr1.fa -a TATAAAAG >> testmatch.res
@@ -22,18 +35,10 @@ echo "" > testmatch.res
 
 
 
-./match -t MMitchell.Gonewiththewind.txt -a "tomorrow is another day" -v 0 >> testmatch.res
-./match -t MMitchell.Gonewiththewind.txt -a "My dear, I don't give a damn" -v 0 >> testmatch.res
-./match -t MMitchell.Gonewiththewind.txt -a "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" -v 0 >> testmatch.res
-./match -t MMitchell.Gonewiththewind.txt -a "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" -v 0 >> testmatch.res
-./match -t MMitchell.Gonewiththewind.txt -a "000000000000000000000000000000000" -v 0 >> testmatch.res
-
-
-
 ./match -t genome/chr1.fa -a AGGCAGTGTGGTTAGCTGGT >> testmatch.res #mir-34
 ./match -t genome/chr1.fa -a TGGAATGTAAAGAAGTATGTA >> testmatch.res #mir-1
 ./match -t genome/chr1.fa -a TCACCGGGTGTACATCAGCTAA >> testmatch.res #mir-40
 ./match -t genome/chr1.fa -a TGAGGTAGTAGGTTGTATAGTT >> testmatch.res #let-7
 ./match -t genome/chr1.fa -a TATCACAGCCAGCTTTGATGTGC >> testmatch.res #mir-2
 
-
+fi
