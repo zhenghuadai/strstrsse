022f8665bdd277b55a07c01241f2ef3aa2dd0ad6
djx.zhenghua <djx.zhenghua@ce1ac406-fc9d-11dd-a7be-4b3ef3b0700c>
2010-10-13 21:22:02 +0000

    thread

--- trunk/strmatch/lib/strstrsse.h
+++ trunk/strmatch/lib/strstrsse.h
@@ -2,7 +2,7 @@
 #ifndef  STRSTRSSE_INC
 #define  STRSTRSSE_INC
 #include "report.h"
-char *lstrstr (const char* , const char* );
-char *strstrsse (const char* , const char* );
+//char *lstrstr (const char* , const char* );
+//char *strstrsse (const char* , const char* );
 
 #endif   /* ----- #ifndef STRSTRSSE_INC  ----- */
